83e20a22e71e70d03cdc70f24b1c0687ae090323
SKYE <azeezabiola@SKYEs-MacBook-Pro.local>
2026-04-11 22:40:28 +0300

    chore: *

--- src/main/java/com/medreminder/medreminder_server/application/dtos/user/AuthResponse.java
+++ src/main/java/com/medreminder/medreminder_server/application/dtos/user/AuthResponse.java
@@ -1,4 +1,4 @@
 package com.medreminder.medreminder_server.application.dtos.user;
 
-public record AuthResponse(String id, String email, String token, String refreshToken) {
+public record AuthResponse(String id, String email, String accessToken, String refreshToken) {
 }
